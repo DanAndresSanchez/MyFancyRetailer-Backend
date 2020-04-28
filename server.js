54e32de0e275e2e8a9f72d0d89861bfcaf443c6e
Dan Sanchez <sanchezd6@students.rowan.edu>
2020-04-28 01:58:45 -0400

    Updated port

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get('/', (req,res) =>{
         })
 })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app is running on port ${process.env.PORT}');
 })
 
 app.post('/signin', (req, res) => {
